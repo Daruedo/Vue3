87aca23da1f1236068356fddb2808b36f5fd1fd7
Lucas Eduardo <daruedo@gmail.com>
2023-06-13 21:03:57 -0300

    Finalizando projeto 4

--- Project 3 - Todo List/scripts.js
+++ Project 3 - Todo List/scripts.js
@@ -14,11 +14,21 @@ const todosApp = {
                 this.newTodo = {
                     done: false
                 };
+                localStorage.setItem("todos", JSON.stringify(this.todos));
             } else {
                 alert("A descrição da tarefa é obrigatória")
             }
+        },
+        storeTodos() {
+            localStorage.setItem("todos", JSON.stringify(this.todos));
         }
-    }
+    },
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
+    },
+//  updated() {
+//      storeTodos()
+//  }
 };
 
 Vue.createApp(todosApp).mount('#app');
